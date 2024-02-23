a86e92190ecd535741c3934ef00a22d04de7e8a5
Jackal_lzt <bkyarokun@gmail.com>
2024-02-23 16:23:06 +0800

    feat: submit todo & rerender

--- VueX/vuex-todo-manager/src/store/modules/todos.js
+++ VueX/vuex-todo-manager/src/store/modules/todos.js
@@ -1,16 +1,39 @@
 import axios from "axios";
 
 const state = {
-	todos: [
-		{ id: 1, title: "todo1" },
-		{ id: 2, title: "todo2" },
-	],
+	todos: [],
 };
 const getters = {
-	allTodos: (store) => store.todos,
+	allTodos: (state) => state.todos,
 };
-const mutations = {};
-const actions = {};
+const actions = {
+	async fetchTodos({ commit }) {
+		const response = await axios.get(
+			"https://jsonplaceholder.typicode.com/todos"
+		);
+		commit("setTodos", response.data);
+	},
+	async addTodo({ commit }, title) {
+		const response = await axios.post(
+			"https://jsonplaceholder.typicode.com/todos",
+			{
+				title,
+				completed: false,
+			}
+		);
+
+		commit("newTodo", response.data);
+	},
+};
+
+const mutations = {
+	setTodos: (state, todos) => {
+		state.todos = todos;
+	},
+	newTodo: (state, todo) => {
+		state.todos.unshift(todo);
+	},
+};
 
 export default {
 	state,
